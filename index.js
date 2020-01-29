18cc5858b56397b54f4754fe8bc75c173edf62e8
Blaise <ifollowjre@gmail.com>
2020-01-28 17:39:07 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
