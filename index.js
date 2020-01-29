705c7a7d8a4e819af3891c8355dd469fabb7b8e3
Blaise <ifollowjre@gmail.com>
2020-01-28 18:05:41 -0700

    ignored files

--- index.js
+++ index.js
@@ -3,13 +3,47 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 const questions = [
-  
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "location",
+        message: "Where are you from?"
+    },
+    {
+        type: "input",
+        name: "hobby",
+        message: "What is your favorite hobby?"
+    },
+    {
+        type: "input",
+        name: "food",
+        message: "What is your favorite food?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username"
+    },
+    {
+        type: "input",
+        name: "linkedin",
+        message: "Enter your LinkedIn URL."
+    }
 ];
 
 function writeToFile(fileName, data) {
- 
+
 }
 
-function init() {
+function promptUser() {
+    return inquirer.prompt(questions);
+}
+promptUser();
 
-init();
+// function init() {
+
+//     init();
